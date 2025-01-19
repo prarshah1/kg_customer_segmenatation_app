203483374e72d1e8b0e29e95d2ecc9073131cc0e
prarshah1 <prarthana.shah@data-axle.com>
2025-01-19 14:08:40 +0530

    making code public temporarily

--- ui_streamlit/neo4j_driver.py
+++ ui_streamlit/neo4j_driver.py
@@ -1,3 +1,5 @@
+import base64
+
 from neo4j import GraphDatabase
 import random
 import ssl
@@ -17,3 +19,15 @@ def get_query_results(query):
     with driver.session() as session:
         result = session.run(query).data()
     return result
+
+def get_image_b64(image_path):
+    if image_path:
+        try:
+            with open(f"{os.getcwd()}/resources/icons/{image_path}", "rb") as img_file:
+                base64_image = "data:image/png;base64," + base64.b64encode(img_file.read()).decode()
+            return base64_image
+        except FileNotFoundError:
+            st.error(f"Image file not found: {image_path}")
+            return None
+    return None
+
